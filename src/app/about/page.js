1c55cb837bc23093c860815694037ba092bdc896
Fathima-Truvoh <fathima@auxvera.com>
2026-05-07 17:24:39 +0530

    auxvera tab name and icon updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,10 @@
 import AboutSection from '@/components/AboutSection'
 import React from 'react'
 
+export const metadata = {
+  title: "About",
+};
+
 function page() {
   return (
     <div>
--- src/app/whyauxvera/page.js
+++ src/app/whyauxvera/page.js
@@ -1,6 +1,10 @@
 import WhyUsSection from '@/components/WhyUsSection'
 import React from 'react'
 
+export const metadata = {
+  title: "Why Us",
+};
+
 function page() {
   return (
     <div>
