f4ace6e3f9b0d84307711270e45cfef1d6529c91
Fathima-Truvoh <fathima@auxvera.com>
2026-05-07 17:26:03 +0530

    code updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,38 +1,4 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import { ThemeProvider } from "next-themes";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata = {
-//   title: "Auxvera Technologies",
-//   description: "",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-
-//         <Navbar/>
-//         {children}
-//         <Footer/>
-//       </body>
-//     </html>
-//   );
-// }
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
